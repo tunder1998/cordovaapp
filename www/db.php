f8fd7bf80006c6b7c2d1f5b7c72265f11531ce1e
lfajarliet <48556102+lfajarliet@users.noreply.github.com>
2019-04-29 19:56:33 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("https://masmandakun.000webhostapp.com","id8973518_fajar","manda","id8973518_akademik") or die ("could not connect database");
+ $con = mysqli_connect("masmandakun.000webhostapp.com","id8973518_fajar","manda","id8973518_akademik") or die ("could not connect database");
 ?>
